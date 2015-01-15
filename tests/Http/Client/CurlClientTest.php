5a06920f1461b6a181bf3a3c7dff856cf6497a10
Issei.M <issei.m7@gmail.com>
2015-01-15 13:48:07 +0900

    separated some test cases into ClientTestCase from CurlClientTest

--- tests/Http/Client/CurlClientTest.php
+++ tests/Http/Client/CurlClientTest.php
@@ -3,51 +3,12 @@
 namespace Spike\Tests\Http\Client;
 
 use Issei\Spike\Http\Client\CurlClient;
+use Issei\Spike\Tests\Http\ClientTestCase;
 
-class CurlClientTest extends \PHPUnit_Framework_TestCase
+class CurlClientTest extends ClientTestCase
 {
-    /**
-     * @var CurlClient
-     */
-    private $SUT;
-
     protected function setUp()
     {
         $this->SUT = new CurlClient();
-    }
-
-    public function testGetRequest()
-    {
-        $response = $this->SUT->request('GET', 'http://httpbin.org/get', '_secret_');
-        $this->assertInstanceOf('Issei\Spike\Http\Response', $response);
-        $this->assertEquals(200, $response->getStatusCode());
-
-        $data = json_decode($response->getBody(), true);
-        $this->assertEquals('Basic ' . base64_encode('_secret_:'), $data['headers']['Authorization']);
-    }
-
-    public function testPostRequest()
-    {
-        $formData = [
-            'foo' => 'bar',
-        ];
-
-        $response = $this->SUT->request('POST', 'http://httpbin.org/post', '_secret_', $formData);
-        $this->assertInstanceOf('Issei\Spike\Http\Response', $response);
-        $this->assertEquals(200, $response->getStatusCode());
-
-        $data = json_decode($response->getBody(), true);
-        $this->assertEquals($formData, $data['form']);
-        $this->assertEquals('Basic ' . base64_encode('_secret_:'), $data['headers']['Authorization']);
-
-    }
-
-    /**
-     * @expectedException     \Issei\Spike\Exception
-     * @expectedExceptionCode 1
-     */
-    public function testRequestWithInvalidUrl()
-    {
-        $this->SUT->request('GET', 'ttp://localhost/', '', []);
     }
 }
