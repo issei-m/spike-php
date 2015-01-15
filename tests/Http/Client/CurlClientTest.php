d247676424493c059e12289eef17d3c5cd801d14
Issei.M <issei.m7@gmail.com>
2015-01-15 18:56:25 +0900

    improved the client tests

--- tests/Http/Client/CurlClientTest.php
+++ tests/Http/Client/CurlClientTest.php
@@ -3,12 +3,105 @@
 namespace Spike\Tests\Http\Client;
 
 use Issei\Spike\Http\Client\CurlClient;
-use Issei\Spike\Tests\Http\ClientTestCase;
 
-class CurlClientTest extends ClientTestCase
+class CurlClientTest extends \PHPUnit_Framework_TestCase
 {
+    /**
+     * @var CurlClient
+     */
+    protected $SUT;
+
     protected function setUp()
     {
         $this->SUT = new CurlClient();
+    }
+
+    public function getUrlProvider()
+    {
+        return [
+            ['http://httpbin.org/get'],
+            ['https://httpbin.org/get'],
+        ];
+    }
+
+    public function postUrlProvider()
+    {
+        return [
+            ['http://httpbin.org/post'],
+            ['https://httpbin.org/post'],
+        ];
+    }
+
+    public function statusCodeProvider()
+    {
+        return [
+            [200],
+            [300],
+            [400],
+            [500],
+        ];
+    }
+
+    public function invalidUrlProvider()
+    {
+        return [
+            ['http://unresolvable-host/'],
+            ['unsupported-protocol://localhost/'],
+        ];
+    }
+
+    /**
+     * @dataProvider getUrlProvider
+     */
+    public function testGetRequest($url)
+    {
+        $queryData = [
+            'foo' => 'bar',
+        ];
+
+        $response = $this->SUT->request('GET', $url . '?' . http_build_query($queryData), '_secret_');
+        $this->assertInstanceOf('Issei\Spike\Http\Response', $response);
+        $this->assertEquals(200, $response->getStatusCode());
+
+        $data = json_decode($response->getBody(), true);
+        $this->assertEquals('Basic ' . base64_encode('_secret_:'), $data['headers']['Authorization']);
+        $this->assertEquals($queryData, $data['args']);
+    }
+
+    /**
+     * @dataProvider postUrlProvider
+     */
+    public function testPostRequest($url)
+    {
+        $formData = [
+            'foo' => 'bar',
+        ];
+
+        $response = $this->SUT->request('POST', $url, '_secret_', $formData);
+        $this->assertInstanceOf('Issei\Spike\Http\Response', $response);
+        $this->assertEquals(200, $response->getStatusCode());
+
+        $data = json_decode($response->getBody(), true);
+        $this->assertEquals('Basic ' . base64_encode('_secret_:'), $data['headers']['Authorization']);
+        $this->assertEquals($formData, $data['form']);
+    }
+
+    /**
+     * @dataProvider statusCodeProvider
+     */
+    public function testStatusCode($statusCode)
+    {
+        $response = $this->SUT->request('GET', 'https://httpbin.org/status/' . $statusCode, '');
+        $this->assertInstanceOf('Issei\Spike\Http\Response', $response);
+        $this->assertEquals($statusCode, $response->getStatusCode());
+    }
+
+    /**
+     * @dataProvider      invalidUrlProvider
+     * @expectedException \Issei\Spike\Exception
+     */
+    public function testRequestWithInvalidUrl($url)
+    {
+        $this->SUT->request('GET', $url, '');
     }
 }
--- tests/Http/Client/GuzzleHttpClientTest.php
+++ tests/Http/Client/GuzzleHttpClientTest.php
@@ -2,14 +2,136 @@
 
 namespace Spike\Tests\Http\Client;
 
-use GuzzleHttp\Client;
+use GuzzleHttp\Exception\BadResponseException;
+use GuzzleHttp\Exception\RequestException;
 use Issei\Spike\Http\Client\GuzzleHttpClient;
-use Issei\Spike\Tests\Http\ClientTestCase;
 
-class GuzzleHttpClientTest extends ClientTestCase
+class GuzzleHttpClientTest extends \PHPUnit_Framework_TestCase
 {
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject
+     */
+    private $client;
+
+    /**
+     * @var GuzzleHttpClient
+     */
+    private $SUT;
+
+    private static $params = [
+        'auth'            => ['_secret_', ''],
+        'connect_timeout' => 10,
+        'timeout'         => 30,
+        'verify'          => true,
+    ];
+
     protected function setUp()
     {
-        $this->SUT = new GuzzleHttpClient(new Client());
+        $this->client = $this->getMock('GuzzleHttp\ClientInterface');
+        $this->SUT = new GuzzleHttpClient($this->client);
+    }
+
+    private function expect_that_client_will_be_called_createRequest_method_with($method, $url, $options)
+    {
+        $request = $this->getMock('GuzzleHttp\Message\RequestInterface');
+
+        $this->client
+            ->expects($this->once())
+            ->method('createRequest')
+            ->with($method, $url, $options)
+            ->will($this->returnValue($request))
+        ;
+
+        return $request;
+    }
+
+    public function testGetRequest()
+    {
+        $request = $this->expect_that_client_will_be_called_createRequest_method_with('GET', 'http://localhost/', self::$params);
+
+        $rawResponse = $this->getMock('GuzzleHttp\Message\ResponseInterface');
+
+        $this->client
+            ->expects($this->once())
+            ->method('send')
+            ->with($this->identicalTo($request))
+            ->will($this->returnValue($rawResponse))
+        ;
+
+        $rawResponse->expects($this->once())->method('getStatusCode')->will($this->returnValue(200));
+        $rawResponse->expects($this->once())->method('getBody')->will($this->returnValue('_body_'));
+
+        $response = $this->SUT->request('GET', 'http://localhost/', '_secret_');
+        $this->assertInstanceOf('Issei\Spike\Http\Response', $response);
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertEquals('_body_', $response->getBody());
+    }
+
+    public function testPostRequest()
+    {
+        $request = $this->expect_that_client_will_be_called_createRequest_method_with('POST', 'http://localhost/', array_merge(self::$params, [
+            'body'            => ['foo' => 'bar'],
+        ]));
+
+        $rawResponse = $this->getMock('GuzzleHttp\Message\ResponseInterface');
+
+        $this->client
+            ->expects($this->once())
+            ->method('send')
+            ->with($this->identicalTo($request))
+            ->will($this->returnValue($rawResponse))
+        ;
+
+        $rawResponse->expects($this->once())->method('getStatusCode')->will($this->returnValue(200));
+        $rawResponse->expects($this->once())->method('getBody')->will($this->returnValue('_body_'));
+
+        $response = $this->SUT->request('POST', 'http://localhost/', '_secret_', ['foo' => 'bar']);
+        $this->assertInstanceOf('Issei\Spike\Http\Response', $response);
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertEquals('_body_', $response->getBody());
+    }
+
+    /**
+     * @test
+     */
+    public function it_should_return_Response_even_if_guzzle_throws_BadResponseException()
+    {
+        $request = $this->expect_that_client_will_be_called_createRequest_method_with('GET', 'http://localhost/', self::$params);
+
+        $rawResponse = $this->getMock('GuzzleHttp\Message\ResponseInterface');
+
+        $this->client
+            ->expects($this->once())
+            ->method('send')
+            ->with($this->identicalTo($request))
+            ->will($this->throwException(new BadResponseException('', $request, $rawResponse)))
+        ;
+
+        $rawResponse->expects($this->once())->method('getStatusCode')->will($this->returnValue(400));
+        $rawResponse->expects($this->once())->method('getBody')->will($this->returnValue('_body_'));
+
+        $response = $this->SUT->request('GET', 'http://localhost/', '_secret_');
+        $this->assertInstanceOf('Issei\Spike\Http\Response', $response);
+        $this->assertEquals(400, $response->getStatusCode());
+        $this->assertEquals('_body_', $response->getBody());
+    }
+
+    /**
+     * @test
+     * @expectedException        \Issei\Spike\Exception
+     * @expectedExceptionMessage error_message
+     */
+    public function it_should_throw_Exception_if_guzzle_throws_RequestException()
+    {
+        $request = $this->expect_that_client_will_be_called_createRequest_method_with('GET', 'http://localhost/', self::$params);
+
+        $this->client
+            ->expects($this->once())
+            ->method('send')
+            ->with($this->identicalTo($request))
+            ->will($this->throwException(new RequestException('error_message', $request, $this->getMock('GuzzleHttp\Message\ResponseInterface'))))
+        ;
+
+        $this->SUT->request('GET', 'http://localhost/', '_secret_');
     }
 }
