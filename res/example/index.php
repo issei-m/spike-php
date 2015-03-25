8dd9a3d29a867273b45df2717e2633b1131f5ad2
Issei.M <issei.m7@gmail.com>
2015-03-25 13:00:55 +0900

    added example

---
+++ res/example/index.php
@@ -0,0 +1,24 @@
+<?php
+
+require __DIR__ . '/../../vendor/autoload.php';
+require __DIR__ . '/definition.php';
+
+$spike = new \Issei\Spike\Spike(API_SECRET);
+
+echo '<h1>New Charge</h1>';
+
+$req = new \Issei\Spike\ChargeRequest();
+$req
+    ->setAmount(100, 'JPY')
+    ->setCard(CARD_TOKEN)
+    ->addProduct(
+        (new \Issei\Spike\Model\Product(uniqid('product-', true)))
+            ->setTitle('Title')
+            ->setDescription('Description')
+            ->setCount(1)
+            ->setPrice(100, 'USD')
+            ->setLanguage('JA')
+    )
+;
+
+dump($spike->charge($req));
