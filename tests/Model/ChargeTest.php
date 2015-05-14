5fd987feaa5994bd8df4a834adf1769932f60312
Issei.M <issei.m7@gmail.com>
2015-05-14 16:11:25 +0900

    fixed ChargeTest

--- tests/Model/ChargeTest.php
+++ tests/Model/ChargeTest.php
@@ -2,7 +2,9 @@
 
 namespace Issei\Spike\Tests\Model;
 
+use Issei\Spike\Model\Card;
 use Issei\Spike\Model\Charge;
+use Issei\Spike\Model\Dispute;
 use Issei\Spike\Model\Money;
 use Issei\Spike\Model\Refund;
 
@@ -22,18 +24,28 @@ class ChargeTest extends \PHPUnit_Framework_TestCase
             ->setCreated(new \DateTime('2015-01-01 10:00:00'))
             ->setPaid(true)
             ->setCaptured(true)
+            ->setSource($card = new Card())
             ->setRefunded(true)
             ->addRefund($refund1 = new Refund())
             ->addRefund($refund2 = new Refund())
+            ->setDispute($dispute = new Dispute())
         ;
         $this->assertEquals('charge', $charge->getId());
         $this->assertEquals(new \DateTime('2015-01-01 10:00:00'), $charge->getCreated());
         $this->assertTrue($charge->isPaid());
         $this->assertTrue($charge->isCaptured());
+        $this->assertSame($card, $charge->getSource());
         $this->assertTrue($charge->isRefunded());
         $this->assertCount(2, $charge->getRefunds());
         $this->assertSame($refund1, $charge->getRefunds()[0]);
         $this->assertSame($refund2, $charge->getRefunds()[1]);
+        $this->assertSame($dispute, $charge->getDispute());
+
+        $charge
+            ->setSource(null)
+            ->setDispute(null);
+        $this->assertNull($charge->getSource(), 'source can be a null');
+        $this->assertNull($charge->getDispute(), 'dispute can be a null');
     }
 
     public function testAmountAccessors()
